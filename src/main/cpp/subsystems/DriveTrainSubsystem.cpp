0e048e474707d950337c44a3603c6b99123eb24f
veritas1236 <112732656+veritas1236@users.noreply.github.com>
2023-01-28 00:19:50 -0700

    Co-authored-by: Alistair Keiller <AlistairKeiller@users.noreply.github.com>

--- src/main/cpp/subsystems/DriveTrainSubsystem.cpp
+++ src/main/cpp/subsystems/DriveTrainSubsystem.cpp
@@ -4,7 +4,100 @@
 
 #include "subsystems/DrivetrainSubsystem.h"
 
-DrivetrainSubsystem::DrivetrainSubsystem();
+using namespace DriveConstants;
+
+DrivetrainSubsystem::DrivetrainSubsystem() {
+    m_leftMaster.ConfigFactoryDefault();
+    m_leftFollower1.ConfigFactoryDefault();
+    m_leftFollower2.ConfigFactoryDefault();
+    m_rightMaster.ConfigFactoryDefault();
+    m_rightFollower1.ConfigFactoryDefault();
+    m_rightFollower2.ConfigFactoryDefault();
+
+    m_leftMaster.SetInverted(kLeftMotorInverted);
+    m_leftFollower1.SetInverted(kLeftMotorInverted);
+    m_leftFollower2.SetInverted(kLeftMotorInverted);
+    m_rightMaster.SetInverted(kRightMotorInverted);
+    m_rightFollower1.SetInverted(kRightMotorInverted);
+    m_rightFollower2.SetInverted(kRightMotorInverted);
+
+    m_leftFollower1.Follow(m_leftMaster);
+    m_leftFollower2.Follow(m_leftMaster);
+    m_rightFollower1.Follow(m_rightMaster);
+    m_rightFollower2.Follow(m_rightMaster);
+
+    m_leftMaster.SetNeutralMode(kNeutralMode);
+    m_leftFollower1.SetNeutralMode(kNeutralMode);
+    m_leftFollower2.SetNeutralMode(kNeutralMode);
+    m_rightMaster.SetNeutralMode(kNeutralMode);
+    m_rightFollower1.SetNeutralMode(kNeutralMode);
+    m_rightFollower2.SetNeutralMode(kNeutralMode);
+
+    ResetEncoders();
+
+    frc::SmartDashboard::PutData("Field", &m_fieldSim);
+};
 
 // This method will be called once per scheduler run
-void DrivetrainSubsystem::Periodic() {}
+void DrivetrainSubsystem::Periodic() {
+    m_odometry.Update(m_IMU.GetRotation2d(),
+        units::meter_t{m_leftMaster.GetSelectedSensorPosition() / kEncoderUnitsPerInch},
+        units::meter_t{m_rightMaster.GetSelectedSensorPosition() / kEncoderUnitsPerInch});
+    m_fieldSim.SetRobotPose(m_odometry.GetPose());
+}
+
+void DrivetrainSubsystem::SimulationPeriodic() {
+    m_drivetrainSimulator.SetInputs(units::volt_t{m_leftMaster.Get()} *
+            frc::RobotController::GetInputVoltage(),
+        units::volt_t{m_rightMaster.Get()} *
+            frc::RobotController::GetInputVoltage());
+    m_drivetrainSimulator.Update(20_ms);
+
+    m_leftEncoderSim.SetDistance(m_drivetrainSimulator.GetLeftPosition().value());
+    m_leftEncoderSim.SetRate(m_drivetrainSimulator.GetLeftVelocity().value());
+    m_rightEncoderSim.SetDistance(m_drivetrainSimulator.GetRightPosition().value());
+    m_rightEncoderSim.SetRate(m_drivetrainSimulator.GetRightVelocity().value());
+    m_IMUSim.SetAngle(-m_drivetrainSimulator.GetHeading().Degrees());
+}
+
+units::ampere_t DrivetrainSubsystem::GetCurrentDraw() const {
+  return m_drivetrainSimulator.GetCurrentDraw();
+}
+
+void DrivetrainSubsystem::ArcadeDrive(double fwd, double rot) {
+  m_drive.ArcadeDrive(fwd, rot);
+}
+
+void DrivetrainSubsystem::ResetEncoders() {
+  m_leftMaster.SetSelectedSensorPosition(0);
+  m_rightMaster.SetSelectedSensorPosition(0);
+}
+
+void DrivetrainSubsystem::SetMaxOutput(double maxOutput) {
+  m_drive.SetMaxOutput(maxOutput);
+}
+
+units::degree_t DrivetrainSubsystem::GetHeading() const {
+  return m_IMU.GetRotation2d().Degrees();
+}
+
+double DrivetrainSubsystem::GetTurnRate() {
+  return -m_IMU.GetRate();
+}
+
+frc::Pose2d DrivetrainSubsystem::GetPose() {
+  return m_odometry.GetPose();
+}
+
+frc::DifferentialDriveWheelSpeeds DrivetrainSubsystem::GetWheelSpeeds() {
+  return {units::meters_per_second_t{m_leftMaster.GetSelectedSensorVelocity()},
+          units::meters_per_second_t{m_rightMaster.GetSelectedSensorVelocity()}};
+}
+
+void DrivetrainSubsystem::ResetOdometry(frc::Pose2d pose) {
+  ResetEncoders();
+  m_drivetrainSimulator.SetPose(pose);
+  m_odometry.ResetPosition(m_gyro.GetRotation2d(),
+                           units::meter_t{m_leftMaster.GetSelectedSensorPosition() / kEncoderUnitsPerInch},
+                           units::meter_t{m_rightMaster.GetSelectedSensorPosition() / kEncoderUnitsPerInch}, pose);
+}
--- src/main/include/subsystems/DriveTrainSubsystem.h
+++ src/main/include/subsystems/DriveTrainSubsystem.h
@@ -4,32 +4,117 @@
 
 #pragma once
 
+// #include <frc/ADXRS450_Gyro.h>
+// #include <frc/motorcontrol/PWMSparkMax.h>
+// #include <frc/simulation/ADXRS450_GyroSim.h>
+#include <Constants.h>
 #include <frc2/command/SubsystemBase.h>
+#include <frc/ADIS16470_IMU.h>
+#include <frc/simulation/ADIS16470_IMUSim.h>
+#include <frc/Encoder.h>
+#include <frc/drive/DifferentialDrive.h>
+#include <frc/geometry/Pose2d.h>
+#include <frc/kinematics/DifferentialDriveOdometry.h>
+#include <frc/motorcontrol/MotorControllerGroup.h>
+#include <frc/simulation/DifferentialDrivetrainSim.h>
+#include <frc/simulation/EncoderSim.h>
+#include <frc/smartdashboard/Field2d.h>
+#include <frc2/command/SubsystemBase.h>
+#include <units/voltage.h>
+
+#include <ctre/Phoenix.h>
 
 class DrivetrainSubsystem : public frc2::SubsystemBase {
  public:
   DrivetrainSubsystem();
 
-      void ArcadeDrive (double forward, double rotation, bool squareInputs);
-      void ResetEncoders();
-      double GetLeftEncoderDistance();
-      double GetRightEncoderDistance();
-      double GetLeftEncoderVelocity();
-      double GetRightEncoderVelocity();
+    units::ampere_t GetCurrentDraw() const;
 
+    /**
+    * Drives the robot using arcade controls.
+    *
+    * @param fwd the commanded forward movement
+    * @param rot the commanded rotation
+    */
+    void ArcadeDrive (double forward, double rotation, bool squareInputs);
 
-  /**
-   * Will be called periodically whenever the CommandScheduler runs.
-   */
-  void Periodic() override;
+    /**
+    * Resets the drive encoders to currently read a position of 0.
+    */
+    void ResetEncoders();
+
+    /**
+    * Returns the heading of the robot.
+    *
+    * @return the robot's heading in degrees, from -180 to 180
+    */
+    units::degree_t GetHeading() const;
+
+    /**
+    * Returns the turn rate of the robot.
+    *
+    * @return The turn rate of the robot, in degrees per second
+    */
+    double GetTurnRate();
+
+    /**
+    * Returns the currently-estimated pose of the robot.
+    *
+    * @return The pose.
+    */
+    frc::Pose2d GetPose();
+
+    /**
+    * Returns the current wheel speeds of the robot.
+    *
+    * @return The current wheel speeds.
+    */
+    frc::DifferentialDriveWheelSpeeds GetWheelSpeeds();
+
+    /**
+    * Resets the odometry to the specified pose.
+    *
+    * @param pose The pose to which to set the odometry.
+    */
+    void ResetOdometry(frc::Pose2d pose);
+
+    /**
+    * Will be called periodically whenever the CommandScheduler runs.
+    */
+    void Periodic() override;
+
+    /**
+    * Will be called periodically during simulation.
+    */
+    void SimulationPeriodic() override;
 
  private:
-      WPI_TalonFX m_leftFront;
-      WPI_TalonFX m_leftMiddle;
-      WPI_TalonFX m_leftBack;
-      WPI_TalonFX m_rightFront;
-      WPI_TalonFX m_rightMiddle;
-      WPI_TalonFX m_rightBack;
+   WPI_TalonFX m_leftMaster{DriveConstants::kLeftMasterID};
+   WPI_TalonFX m_leftFollower1{DriveConstants::kLeftFollower1ID};
+   WPI_TalonFX m_leftFollower2{DriveConstants::kLeftFollower2ID};
+   WPI_TalonFX m_rightMaster{DriveConstants::kRightMasterID};
+   WPI_TalonFX m_rightFollower1{DriveConstants::kRightFollower1ID};
+   WPI_TalonFX m_rightFollower2{DriveConstants::kRightFollower2ID};
 
-      frc::DifferentialDrive m_drive{m_leftFront, m_rightFront};
+   TalonFXSimCollection& m_leftMasterSim{m_leftMaster};
+	TalonFXSimCollection& m_rightMasterSim{m_rightMaster};
+
+   frc::DifferentialDrive m_drive{m_leftMaster, m_rightMaster};
+
+   frc::ADIS16470_IMU m_IMU;
+
+   frc::sim::ADIS16470_IMUSim m_IMUSim{m_IMU};
+
+   frc::DifferentialDriveOdometry m_odometry; // could init, IDK
+
+   frc::sim::DifferentialDrivetrainSim m_drivetrainSimulator{ // come back to drivetrain plant later
+      frc::DCMotor::Falcon500(3),
+      DriveConstants::kDrivetrainGearing,
+      DriveConstants::MOI,
+      DriveConstants::kMass,
+      DriveConstants::kWheelDiameter / 2,
+      DriveConstants::kTrackwidth,
+      {0.001, 0.001, 0.001, 0.1, 0.1, 0.005, 0.005}};
+
+   frc::Field2d m_fieldSim;
 };
