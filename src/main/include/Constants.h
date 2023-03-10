d99018df66bf3a01997e288ddb531910fb3232cc
Alistair Keiller <alistair@keiller.net>
2023-03-09 19:58:41 -0800

    update

--- src/main/include/Constants.h
+++ src/main/include/Constants.h
@@ -2,16 +2,46 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-#include <frc/kinematics/DifferentialDriveKinematics.h>
-#include <frc/system/plant/DCMotor.h>
-#include <frc/system/plant/LinearSystemId.h>
-#include <frc/trajectory/constraint/DifferentialDriveKinematicsConstraint.h>
+// all units
 #include <units/acceleration.h>
 #include <units/angle.h>
+#include <units/angular_acceleration.h>
+#include <units/angular_velocity.h>
+#include <units/area.h>
+#include <units/capacitance.h>
+#include <units/charge.h>
+#include <units/concentration.h>
+#include <units/conductance.h>
+#include <units/current.h>
+#include <units/curvature.h>
+#include <units/data.h>
+#include <units/data_transfer_rate.h>
+#include <units/density.h>
+#include <units/dimensionless.h>
+#include <units/energy.h>
+#include <units/force.h>
+#include <units/frequency.h>
+#include <units/illuminance.h>
+#include <units/impedance.h>
+#include <units/inductance.h>
 #include <units/length.h>
+#include <units/luminous_flux.h>
+#include <units/luminous_intensity.h>
+#include <units/magnetic_field_strength.h>
+#include <units/magnetic_flux.h>
+#include <units/mass.h>
+#include <units/moment_of_inertia.h>
+#include <units/power.h>
+#include <units/pressure.h>
+#include <units/radiation.h>
+#include <units/solid_angle.h>
+#include <units/substance.h>
+#include <units/temperature.h>
 #include <units/time.h>
+#include <units/torque.h>
 #include <units/velocity.h>
 #include <units/voltage.h>
+#include <units/volume.h>
 
 #include <ctre/Phoenix.h>
 
